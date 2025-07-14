a82dc610b3f3070b3d665f24cbd199104814c64a
atanilson <129515833+atanilson@users.noreply.github.com>
2025-07-14 11:07:17 +0100

    Update engine.py

--- scripts/engine.py
+++ scripts/engine.py
@@ -5,6 +5,8 @@ import torch
 from tqdm.auto import tqdm
 from typing import Dict, List
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
